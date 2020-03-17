cdb6b6c99a558fa9c29422676a28f18ed0a42077
Hoyame Z <56686012+corazon2@users.noreply.github.com>
2020-03-17 22:13:01 +0100

    Delete sv_game.lua

--- corazon/game/sv_game.lua
+++
@@ -1,8 +0,0 @@
-RegisterServerEvent('corazon:ShowServerMeID')
-AddEventHandler('corazon:ShowServerMeID', function()
-	local license = GetPlayerIdentifiers(source)[1]
-	local source = source	
-    local result = MySQL.Sync.fetchAll("SELECT firstConnection FROM joueurs WHERE license = @license", {['@license'] = license})
-    
-	TriggerClientEvent('corazon.game:gripData', source, result)
-end)
